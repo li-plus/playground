785da640f12e9cf334e976cf0a6f284bc0329eab
lijiahao <liplus17@163.com>
2022-03-04 22:19:01 +0800

    add prefetch

--- src/gemm.cu
+++ src/gemm.cu
@@ -52,51 +52,8 @@ __device__ static inline float4 &operator+=(float4 &self, const float4 &other) {
 
 __device__ static inline float4 operator*(const float4 &a, float s) { return {a.x * s, a.y * s, a.z * s, a.w * s}; }
 
+template <int BLOCK_SIZE_M, int BLOCK_SIZE_N, int BLOCK_SIZE_K, int THREAD_SIZE_X, int THREAD_SIZE_Y, bool DO_PREFETCH>
 __global__ void sgemm3_kernel(int M, int N, int K, const float *__restrict__ A, const float *__restrict__ B,
-                              float *__restrict__ C) {
-    constexpr int BLOCK_SIZE_X = 64;
-    constexpr int THREAD_SIZE_X = 4;
-    constexpr int BLOCK_SIZE_Y = 64;
-    constexpr int THREAD_SIZE_Y = 4;
-
-    const int blk_x = threadIdx.x * THREAD_SIZE_X;
-    const int blk_y = threadIdx.y * THREAD_SIZE_Y;
-    const int x = blockIdx.x * BLOCK_SIZE_X + blk_x;
-    const int y = blockIdx.y * BLOCK_SIZE_Y + blk_y;
-
-    __shared__ float shA[BLOCK_SIZE_Y][BLOCK_SIZE_X];
-    __shared__ float shB[BLOCK_SIZE_Y][BLOCK_SIZE_X];
-
-    float4 sums[THREAD_SIZE_Y] = {};
-    for (int k = 0; k < K; k += BLOCK_SIZE_X) {
-#pragma unroll
-        for (int thr_y = 0; thr_y < THREAD_SIZE_Y; thr_y++) {
-            *(float4 *)&shA[blk_y + thr_y][blk_x] = *(float4 *)&A[(y + thr_y) * K + (k + blk_x)];
-            *(float4 *)&shB[blk_y + thr_y][blk_x] = *(float4 *)&B[(k + blk_y + thr_y) * N + x];
-        }
-        __syncthreads();
-#pragma unroll
-        for (int blk_k = 0; blk_k < BLOCK_SIZE_X; blk_k += THREAD_SIZE_X) {
-#pragma unroll
-            for (int thr_y = 0; thr_y < 4; thr_y++) {
-                float4 thA = *(float4 *)&shA[blk_y + thr_y][blk_k];
-#pragma unroll
-                for (int thr_k = 0; thr_k < 4; thr_k++) {
-                    float4 thB = *(float4 *)&shB[blk_k + thr_k][blk_x];
-                    sums[thr_y] += thB * ((float *)&thA)[thr_k];
-                }
-            }
-        }
-        __syncthreads();
-    }
-#pragma unroll
-    for (int thr_y = 0; thr_y < THREAD_SIZE_Y; thr_y++) {
-        *(float4 *)&C[(y + thr_y) * N + x] = sums[thr_y];
-    }
-}
-
-template <int BLOCK_SIZE_M, int BLOCK_SIZE_N, int BLOCK_SIZE_K, int THREAD_SIZE_X, int THREAD_SIZE_Y>
-__global__ void sgemm4_kernel(int M, int N, int K, const float *__restrict__ A, const float *__restrict__ B,
                               float *__restrict__ C) {
 #define A_AT(row, col) A[(row)*K + (col)]
 #define B_AT(row, col) B[(row)*N + (col)]
@@ -132,31 +89,77 @@ __global__ void sgemm4_kernel(int M, int N, int K, const float *__restrict__ A, 
 
     float4 sums[THREAD_SIZE_Y] = {};
 
-    for (int k = 0; k < K; k += BLOCK_SIZE_K) {
+    if (!DO_PREFETCH) {
+        for (int k = 0; k < K; k += BLOCK_SIZE_K) {
 #pragma unroll
-        for (int A_blk_y = A_blk_start_y; A_blk_y < BLOCK_SIZE_M; A_blk_y += A_NUM_THREADS_Y) {
-            *(float4 *)&shA[A_blk_y][A_blk_x] = *(float4 *)&A_AT(by * BLOCK_SIZE_M + A_blk_y, k + A_blk_x);
-        }
+            for (int A_blk_y = A_blk_start_y; A_blk_y < BLOCK_SIZE_M; A_blk_y += A_NUM_THREADS_Y) {
+                *(float4 *)&shA[A_blk_y][A_blk_x] = *(float4 *)&A_AT(by * BLOCK_SIZE_M + A_blk_y, k + A_blk_x);
+            }
 #pragma unroll
-        for (int B_blk_y = B_blk_start_y; B_blk_y < BLOCK_SIZE_K; B_blk_y += B_NUM_THREADS_Y) {
-            *(float4 *)&shB[B_blk_y][B_blk_x] = *(float4 *)&B_AT(k + B_blk_y, bx * BLOCK_SIZE_N + B_blk_x);
-        }
-        __syncthreads();
+            for (int B_blk_y = B_blk_start_y; B_blk_y < BLOCK_SIZE_K; B_blk_y += B_NUM_THREADS_Y) {
+                *(float4 *)&shB[B_blk_y][B_blk_x] = *(float4 *)&B_AT(k + B_blk_y, bx * BLOCK_SIZE_N + B_blk_x);
+            }
+            __syncthreads();
 
 #pragma unroll
-        for (int blk_k = 0; blk_k < BLOCK_SIZE_K; blk_k += THREAD_SIZE_X) {
+            for (int blk_k = 0; blk_k < BLOCK_SIZE_K; blk_k += THREAD_SIZE_X) {
 #pragma unroll
-            for (int thr_y = 0; thr_y < THREAD_SIZE_Y; thr_y++) {
-                float4 thA = *(float4 *)&shA[ty * THREAD_SIZE_Y + thr_y][blk_k];
+                for (int thr_y = 0; thr_y < THREAD_SIZE_Y; thr_y++) {
+                    float4 thA = *(float4 *)&shA[ty * THREAD_SIZE_Y + thr_y][blk_k];
 #pragma unroll
-                for (int thr_k = 0; thr_k < 4; thr_k++) {
-                    float4 thB = *(float4 *)&shB[blk_k + thr_k][tx * THREAD_SIZE_X];
-                    sums[thr_y] += thB * ((float *)&thA)[thr_k];
+                    for (int thr_k = 0; thr_k < 4; thr_k++) {
+                        float4 thB = *(float4 *)&shB[blk_k + thr_k][tx * THREAD_SIZE_X];
+                        sums[thr_y] += thB * ((float *)&thA)[thr_k];
+                    }
+                }
+            }
+            __syncthreads();
+        }
+    } else {
+        static_assert(B_NUM_THREADS_Y % THREAD_SIZE_X == 0);
+
+        float4 next_B;
+
+#pragma unroll
+        for (int B_blk_y = B_blk_start_y; B_blk_y < BLOCK_SIZE_K; B_blk_y += B_NUM_THREADS_Y) {
+            *(float4 *)&shB[B_blk_y][B_blk_x] = *(float4 *)&B_AT(B_blk_y, bx * BLOCK_SIZE_N + B_blk_x);
+        }
+
+        for (int k = 0; k < K; k += BLOCK_SIZE_K) {
+#pragma unroll
+            for (int A_blk_y = A_blk_start_y; A_blk_y < BLOCK_SIZE_M; A_blk_y += A_NUM_THREADS_Y) {
+                *(float4 *)&shA[A_blk_y][A_blk_x] = *(float4 *)&A_AT(by * BLOCK_SIZE_M + A_blk_y, k + A_blk_x);
+            }
+
+            const int next_k = (k + BLOCK_SIZE_K < K) ? k + BLOCK_SIZE_K : 0;
+            __syncthreads();
+
+#pragma unroll
+            for (int blk_k = 0; blk_k < BLOCK_SIZE_K; blk_k += THREAD_SIZE_X) {
+                // prefetch global memory to register
+                const int B_blk_y = B_blk_start_y + B_NUM_THREADS_Y * (blk_k / B_NUM_THREADS_Y);
+                if (blk_k % B_NUM_THREADS_Y == 0) {
+                    next_B = *(float4 *)&B_AT(next_k + B_blk_y, bx * BLOCK_SIZE_N + B_blk_x);
+                }
+                // compute
+#pragma unroll
+                for (int thr_y = 0; thr_y < THREAD_SIZE_Y; thr_y++) {
+                    float4 thA = *(float4 *)&shA[ty * THREAD_SIZE_Y + thr_y][blk_k];
+#pragma unroll
+                    for (int thr_k = 0; thr_k < THREAD_SIZE_X; thr_k++) {
+                        float4 thB = *(float4 *)&shB[blk_k + thr_k][tx * THREAD_SIZE_X];
+                        sums[thr_y] += thB * ((float *)&thA)[thr_k];
+                    }
+                }
+                // store to shared memory
+                if ((blk_k + THREAD_SIZE_X) % B_NUM_THREADS_Y == 0) {
+                    __syncthreads();
+                    *(float4 *)&shB[B_blk_y][B_blk_x] = next_B;
                 }
             }
         }
-        __syncthreads();
     }
+
 #pragma unroll
     for (int thr_y = 0; thr_y < THREAD_SIZE_Y; thr_y++) {
         *(float4 *)&C_AT(by * BLOCK_SIZE_M + ty * THREAD_SIZE_Y + thr_y, bx * BLOCK_SIZE_N + tx * THREAD_SIZE_X) =
@@ -170,45 +173,40 @@ __global__ void sgemm4_kernel(int M, int N, int K, const float *__restrict__ A, 
 
 static inline int ceiling(int a, int b) { return (a + b - 1) / b; }
 
-static inline void sgemm1_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
+static inline cudaError_t sgemm1_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
     constexpr int block_size = 32;
     dim3 blocks(ceiling(N, block_size), ceiling(M, block_size));
     dim3 threads(block_size, block_size);
     sgemm1_kernel<<<blocks, threads>>>(M, N, K, A, B, C);
+    return cudaSuccess;
 }
 
-static inline void sgemm2_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
+static inline cudaError_t sgemm2_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
     constexpr int block_size = 32;
     dim3 blocks(ceiling(N, block_size), ceiling(M, block_size));
     dim3 threads(block_size, block_size);
     sgemm2_kernel<<<blocks, threads>>>(M, N, K, A, B, C);
+    return cudaSuccess;
 }
 
-static inline void sgemm3_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
-    constexpr int block_size = 64;
-    constexpr int thread_size_x = block_size / 4;
-    constexpr int thread_size_y = block_size / 4;
-    dim3 blocks(ceiling(N, block_size), ceiling(M, block_size));
-    dim3 threads(thread_size_x, thread_size_y);
-    sgemm3_kernel<<<blocks, threads>>>(M, N, K, A, B, C);
-}
-
-static inline void sgemm4_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
+static inline cudaError_t sgemm3_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
     constexpr int BLOCK_SIZE_M = 32;
     constexpr int BLOCK_SIZE_N = 64;
     constexpr int BLOCK_SIZE_K = 32;
     constexpr int THREAD_SIZE_X = 4;
     constexpr int THREAD_SIZE_Y = 4;
+    constexpr bool DO_PREFETCH = true;
     if (N % BLOCK_SIZE_N != 0 || M % BLOCK_SIZE_M != 0 || K % BLOCK_SIZE_K != 0) {
-        throw std::invalid_argument("invalid matrix dimensions");
+        fprintf(stderr, "invalid matrix dimensions: M=%d, N=%d, K=%d\n", M, N, K);
+        return cudaErrorInvalidValue;
     }
     dim3 blocks(N / BLOCK_SIZE_N, M / BLOCK_SIZE_M);
     dim3 threads(BLOCK_SIZE_N / THREAD_SIZE_X, BLOCK_SIZE_M / THREAD_SIZE_Y);
-    sgemm4_kernel<BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K, THREAD_SIZE_X, THREAD_SIZE_Y>
+    sgemm3_kernel<BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K, THREAD_SIZE_X, THREAD_SIZE_Y, DO_PREFETCH>
         <<<blocks, threads>>>(M, N, K, A, B, C);
+    return cudaSuccess;
 }
 
 cudaError_t sgemm_cuda(int M, int N, int K, const float *A, const float *B, float *C) {
-    sgemm4_cuda(M, N, K, A, B, C);
-    return cudaSuccess;
+    return sgemm3_cuda(M, N, K, A, B, C);
 }
--- src/tune.sh
+++ src/tune.sh
@@ -5,19 +5,20 @@ mkdir -p $root/build
 cd $root/build
 cmake ..
 
-for M in 32 64 128; do
-    for N in 32 64 128; do
-        for K in 32 64 128; do
+for BM in 32 64 128; do
+    for BN in 32 64 128; do
+        for BK in 32 64 128; do
             for TY in 1 2 4; do
-                echo "M=$M, N=$N, K=$K, TY=$TY"
-                sed -e "s/^.*constexpr int BLOCK_SIZE_M.*$/    constexpr int BLOCK_SIZE_M = $M;/g" \
-                    -e "s/^.*constexpr int BLOCK_SIZE_N.*$/    constexpr int BLOCK_SIZE_N = $N;/g" \
-                    -e "s/^.*constexpr int BLOCK_SIZE_K.*$/    constexpr int BLOCK_SIZE_K = $K;/g" \
-                    -e "s/^.*constexpr int THREAD_SIZE_Y.*$/    constexpr int THREAD_SIZE_Y = $TY;/g" \
-                    -i ../src/gemm.cu
-                make -j >/dev/null 2>&1
-                if [[ $? -ne 0 ]]; then continue; fi
-                ./bin/gemm_bench
+                for PREFETCH in "false" "true"; do
+                    echo "BM=$BM, BN=$BN, BK=$BK, TY=$TY, PREFETCH=$PREFETCH"
+                    sed -e "s/^.*constexpr int BLOCK_SIZE_M.*$/    constexpr int BLOCK_SIZE_M = $BM;/g" \
+                        -e "s/^.*constexpr int BLOCK_SIZE_N.*$/    constexpr int BLOCK_SIZE_N = $BN;/g" \
+                        -e "s/^.*constexpr int BLOCK_SIZE_K.*$/    constexpr int BLOCK_SIZE_K = $BK;/g" \
+                        -e "s/^.*constexpr int THREAD_SIZE_Y.*$/    constexpr int THREAD_SIZE_Y = $TY;/g" \
+                        -e "s/^.*constexpr bool DO_PREFETCH.*$/    constexpr bool DO_PREFETCH = $PREFETCH;/g" \
+                        -i ../src/gemm.cu
+                    make -j >/dev/null 2>&1 && ./bin/gemm_bench
+                done
             done
         done
     done
