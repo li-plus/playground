04cdc0a81f598a0d80ab8843999ff89ef924d570
Jiahao Li <liplus17@163.com>
2024-05-24 14:21:42 +0800

    fix multi_stream demo

--- pytorch/multi_stream.py
+++ pytorch/multi_stream.py
@@ -3,13 +3,13 @@ Compute small GEMMs using single stream vs multiple streams.
 
 nsys profile python3 multi_stream.py
 
-<torch.utils.benchmark.utils.common.Measurement object at 0x7f184e933b20>
+<torch.utils.benchmark.utils.common.Measurement object at 0x7f869b527e50>
 single_stream(matrices)
-  11.59 ms
+  3.36 ms
   1 measurement, 10 runs , 1 thread
-<torch.utils.benchmark.utils.common.Measurement object at 0x7f18d4e67ac0>
+<torch.utils.benchmark.utils.common.Measurement object at 0x7f872e6cb490>
 multi_stream(matrices, streams)
-  10.11 ms
+  2.22 ms
   1 measurement, 10 runs , 1 thread
 """
 
@@ -23,15 +23,15 @@ def single_stream(matrices):
 
 
 def multi_stream(matrices, streams):
-    num_gemms_per_stream = (len(matrices) + len(streams)) // len(streams)
-    for i, stream in enumerate(streams):
+    for stream in streams:
         with torch.cuda.stream(stream):
-            for i, mat in enumerate(matrices[i * num_gemms_per_stream : (i + 1) * num_gemms_per_stream]):
+            for i in range(0, len(matrices), len(streams)):
+                mat = matrices[i]
                 mat @ mat.T
 
 
 torch.cuda.set_device(0)
-matrices = [torch.randn(2048, 128, device="cuda") for _ in range(128)]
+matrices = [torch.randn(1024, 128, device="cuda") for _ in range(128)]
 streams = [torch.cuda.Stream(torch.cuda.current_device()) for _ in range(2)]
 
 print(Timer(stmt="single_stream(matrices)", globals=globals()).timeit(10))
