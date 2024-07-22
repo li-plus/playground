5fd203432f85a9c02099adbad828729151f6f3ba
Jiahao Li <liplus17@163.com>
2024-07-22 14:34:41 +0800

    Default init

--- cpp/init.cpp
+++ cpp/init.cpp
@@ -1,11 +1,24 @@
 #include <cstring>
 #include <iostream>
 
-class A {
-  public:
+struct A {
     A() = default; // will ptr be initialized to nullptr by default? NO!
 
     int *ptr;
+};
+
+struct DefaultInit {
+    struct Object {
+        Object(int val) { printf("Object init with value %d\n", val); }
+    };
+
+    DefaultInit() {}
+
+    DefaultInit(int val) : obj(val) {}
+
+    DefaultInit(int val, bool) { obj = Object(val); }
+
+    Object obj = Object(0);
 };
 
 int main() {
@@ -16,10 +29,19 @@ int main() {
         A a;
         printf("a.ptr: %p\n", a.ptr);
     }
-
     {
         A a{};
         printf("a.ptr: %p\n", a.ptr);
+    }
+
+    // default initialization
+    {
+        printf("Creating a\n");
+        DefaultInit a; // prints 0
+        printf("Creating b\n");
+        DefaultInit b(1); // prints 1
+        printf("Creating c\n");
+        DefaultInit c(1, true); // prints 0 & 1
     }
 
     return 0;
