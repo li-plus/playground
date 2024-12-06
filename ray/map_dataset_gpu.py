ae23d39b9ef4d34eb9c6ecf15a41cdd3b2cf32c9
Jiahao Li <liplus17@163.com>
2024-12-06 21:56:34 +0800

    update generator

--- ray/map_dataset_gpu.py
+++ ray/map_dataset_gpu.py
@@ -4,6 +4,8 @@ Stateful transform: https://docs.ray.io/en/latest/data/transforming-data.html#st
 
 from __future__ import annotations
 
+import json
+
 import numpy as np
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel
@@ -12,38 +14,55 @@ import ray
 import ray.data
 
 
-class TorchPredictor:
-    def __init__(self):
-        model_name_or_path = "THUDM/glm-4-9b-chat"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
+class Generator:
+    def __init__(self, model_id: str) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side="left", trust_remote_code=True)
         self.model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
-            model_name_or_path,
-            torch_dtype=torch.half,
+            model_id,
+            attn_implementation="flash_attention_2",
+            torch_dtype=torch.bfloat16,
             device_map="auto",
+            use_cache=True,
             low_cpu_mem_usage=True,
             trust_remote_code=True,
         ).eval()
 
     def __call__(self, batch: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
-        batch_messages = [
-            [{"role": "user", "content": f"Count from {s} to {e}: "}] for s, e in zip(batch["start"], batch["end"])
-        ]
+        conversations = [json.loads(conv) for conv in batch["conversation"]]
         inputs = self.tokenizer.apply_chat_template(
-            batch_messages, add_generation_prompt=True, tokenize=True, return_tensors="pt", return_dict=True
+            conversations,
+            add_generation_prompt=True,
+            tokenize=True,
+            padding=True,
+            return_tensors="pt",
+            return_dict=True,
         ).to("cuda")
 
-        outputs = self.model.generate(**inputs, do_sample=False)
-        outputs = outputs[:, inputs["input_ids"].shape[1] :]
-        output_texts = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        output_texts = [x.strip() for x in output_texts]
-        batch["output"] = output_texts
+        sequence_ids = self.model.generate(**inputs, do_sample=False, max_new_tokens=256)
+
+        output_ids = sequence_ids[:, inputs["input_ids"].shape[1] :].cpu()
+        output_texts = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+        for conv, output_text in zip(conversations, output_texts):
+            conv.append({"role": "assistant", "content": output_text})
+        batch["conversation"] = [json.dumps(conv) for conv in conversations]
         return batch
 
 
-ranges = [(start, end) for start in range(100) for end in range(start, 100)]
-print(f"dataset size {len(ranges)}")
-ds = ray.data.from_items([{"start": s, "end": e} for s, e in ranges])
-ds = ds.map_batches(TorchPredictor, batch_size=16, concurrency=8, num_gpus=1)
+model_id = "Qwen/Qwen2.5-0.5B-Instruct"
+batch_size = 64
+conversations = [
+    {"conversation": json.dumps([{"role": "user", "content": f"Count from {start} to {end}: "}])}
+    for start in range(100)
+    for end in range(start, 100)
+]
+print(f"dataset size {len(conversations)}")
+override_num_blocks = (len(conversations) + batch_size - 1) // batch_size
+ray.init()
+concurrency = int(ray.available_resources().get("GPU"))
+ds = ray.data.from_items(conversations, override_num_blocks=override_num_blocks)
+ds = ds.map_batches(
+    Generator, fn_constructor_kwargs=dict(model_id=model_id), batch_size=batch_size, concurrency=concurrency, num_gpus=1
+)
 df = ds.to_pandas()
 df.to_csv("gen.csv")
 print(df.head(100))
