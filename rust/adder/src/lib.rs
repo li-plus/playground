1d3910086f8abfa334313924eea1635f518e6aa0
Jiahao Li <liplus17@163.com>
2025-08-16 19:05:46 +0800

    update

--- rust/adder/src/lib.rs
+++ rust/adder/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn do_panic() {
+    panic!("This function always panics");
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,23 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn it_fails() {
+        let result = add(2, 2);
+        // assert with custom error message
+        assert_eq!(
+            result, 5,
+            "Expected 2 + 2 to equal 5, but it equals {}",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "always panics")] // substring of the message
+    fn it_panics() {
+        do_panic();
     }
 }
