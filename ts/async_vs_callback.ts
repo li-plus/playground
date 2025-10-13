201459c39a0bbcefd544f674743b3caf786914d2
Jiahao Li <liplus17@163.com>
2025-10-13 14:28:25 +0800

    update ts demo

--- ts/async_vs_callback.ts
+++ ts/async_vs_callback.ts
@@ -1,24 +1,24 @@
-function taskWithCallback(callback: (result: number) => void) {
-    const workload = Math.random() * 1000
-    console.log(`task with callback started`);
-    setTimeout(() => callback(workload), workload)
-}
-
-function asyncTask(): Promise<number> {
-    const workload = Math.random() * 1000
-    return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(workload), workload)
-    })
-}
-
 async function main() {
-    console.log('async task started')
-    const workload = await asyncTask();
-    console.log(`async task finished within ${workload.toFixed(2)} ms`);
+  {
+    // task with callback
+    console.log('task with callback started');
+    setTimeout(() => {
+      console.log('task with callback finished');
+    }, 1000);
+  }
 
-    taskWithCallback((result) => {
-        console.log(`task with callback finished within ${result.toFixed(2)} ms`);
-    });
+  {
+    // promise then
+    console.log('async task started');
+    new Promise((resolve) => setTimeout(resolve, 1000)).then((result) => console.log('async task finished'));
+  }
+
+  {
+    // best practise: async task
+    console.log('async task started');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log('async task finished');
+  }
 }
 
 main()
