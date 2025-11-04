37eb1826488cd33f91f96733f574c56d706ad361
Jiahao Li <liplus17@163.com>
2025-11-04 16:38:57 +0800

    add llm proxy rs

--- rust/llm_client/src/main.rs
+++ rust/llm_client/src/main.rs
@@ -1,14 +1,17 @@
 use dotenvy::dotenv;
+use futures::StreamExt;
 use reqwest;
-use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
+use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::io::{stdout, Write};
 use tokio;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ChatRequest {
     model: String,
     messages: Vec<Message>,
+    stream: Option<bool>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,6 +30,92 @@ struct Choice {
     message: Message,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct ChunkResponse {
+    choices: Vec<DeltaChoice>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DeltaChoice {
+    delta: DeltaMessage,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DeltaMessage {
+    role: Option<String>,
+    content: Option<String>,
+}
+
+async fn chat(
+    client: &reqwest::Client,
+    api_url: &String,
+    chat_request: &ChatRequest,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let response = client.post(api_url).json(chat_request).send().await?;
+
+    if !response.status().is_success() {
+        return Err(format!(
+            "Error: {}. Info: {}",
+            response.status(),
+            response.text().await?
+        )
+        .into());
+    }
+
+    // sync chat
+    if !chat_request.stream.unwrap_or(false) {
+        let chat_response: ChatResponse = response.json().await?;
+        chat_response.choices.iter().for_each(|choice| {
+            println!("{}: {}", choice.message.role, choice.message.content);
+        });
+        return Ok(());
+    }
+
+    // streaming
+    let mut stream = response.bytes_stream();
+    let mut buffer = String::new();
+
+    while let Some(chunk) = stream.next().await {
+        let chunk = chunk?;
+        let chunk_str = String::from_utf8_lossy(&chunk);
+        buffer.push_str(&chunk_str);
+
+        while let Some(pos) = buffer.find("\n\n") {
+            let line = buffer[..pos].to_string();
+            buffer = buffer[pos + 2..].to_string();
+
+            if line.starts_with("data: ") {
+                let data = &line[6..];
+
+                // println!("data: {:#?}", data);
+
+                if data == "[DONE]" {
+                    println!(); // New line after streaming
+                    return Ok(());
+                }
+
+                match serde_json::from_str::<ChunkResponse>(data) {
+                    Ok(chunk) => {
+                        chunk
+                            .choices
+                            .into_iter()
+                            .filter_map(|choice| choice.delta.content)
+                            .for_each(|content| {
+                                print!("{}", content);
+                                stdout().flush().unwrap();
+                            });
+                    }
+                    Err(e) => {
+                        eprintln!("Error parsing JSON: {} - Data: {}", e, data);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
@@ -34,35 +123,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     const DEFAULT_BASE_URL: &str = "https://api.openai.com/v1";
 
     let base_url = env::var("OPENAI_BASE_URL").unwrap_or(String::from(DEFAULT_BASE_URL));
+    let api_url = base_url + "/chat/completions";
     let api_key = env::var("OPENAI_API_KEY").unwrap();
     let model_name = env::var("MODEL_NAME").unwrap();
 
-    let chat_request = ChatRequest {
+    let mut chat_request = ChatRequest {
         model: model_name,
         messages: vec![Message {
             role: String::from("user"),
-            content: String::from("Hello, how are you?"),
+            content: String::from("Write a quick sort function in rust"),
         }],
+        stream: None,
     };
     println!(
         "{}: {}",
         chat_request.messages[0].role, chat_request.messages[0].content
     );
 
-    let client = reqwest::Client::new();
-    let response = client
-        .post(base_url + "/chat/completions")
-        .json(&chat_request)
-        .header(AUTHORIZATION, format!("Bearer {}", api_key))
-        .header(CONTENT_TYPE, "application/json")
-        .send()
-        .await?;
-
-    let chat_response: ChatResponse = response.json().await?;
-    println!(
-        "{}: {}",
-        chat_response.choices[0].message.role, chat_response.choices[0].message.content
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        format!("Bearer {}", api_key).parse().unwrap(),
     );
+    headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    // sync
+    // chat(&client, &api_url, &chat_request).await?;
+
+    // streaming
+    chat_request.stream = Some(true);
+    chat(&client, &api_url, &chat_request).await?;
 
     Ok(())
 }
---
+++ rust/llm_proxy/src/main.rs
@@ -0,0 +1,114 @@
+use axum::{
+    body::Body,
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    Json, Router,
+};
+use config::{Config, File};
+use mime::APPLICATION_JSON;
+use reqwest::{
+    header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE},
+    Client,
+};
+use serde::Deserialize;
+use serde_json::Value as JsonValue;
+use std::{collections::HashMap, sync::Arc};
+use tracing::info;
+
+struct AppState {
+    client: Client,
+    config: AppConfig,
+}
+
+#[derive(Deserialize)]
+struct ProviderConfig {
+    api_url: String,
+    api_key: String,
+    model: String,
+}
+
+#[derive(Deserialize)]
+struct AppConfig {
+    providers: HashMap<String, ProviderConfig>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    let config: AppConfig = Config::builder()
+        .add_source(File::with_name("config/default"))
+        .build()?
+        .try_deserialize()?;
+
+    let addr = "0.0.0.0:8080";
+
+    let client = reqwest::Client::new();
+
+    let state = Arc::new(AppState { client, config });
+
+    let app = Router::new()
+        .route("/chat/completions", post(chat_completions))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+async fn chat_completions(
+    State(app_state): State<Arc<AppState>>,
+    request: Json<JsonValue>,
+) -> Response {
+    let mut request = request.0;
+
+    let Some(JsonValue::String(model_ref)) = request.get_mut("model") else {
+        return StatusCode::BAD_REQUEST.into_response();
+    };
+
+    let Some(provider) = app_state.config.providers.get(model_ref) else {
+        return StatusCode::NOT_FOUND.into_response();
+    };
+    *model_ref = provider.model.clone();
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        format!("Bearer {}", provider.api_key).parse().unwrap(),
+    );
+    headers.insert(CONTENT_TYPE, APPLICATION_JSON.as_ref().parse().unwrap());
+
+    info!("Headers: {:#?}", headers);
+
+    info!("Received payload: {:#?}", request);
+
+    let Ok(upstream_response) = app_state
+        .client
+        .post(&provider.api_url)
+        .headers(headers)
+        .json(&request)
+        .send()
+        .await
+    else {
+        return StatusCode::BAD_GATEWAY.into_response();
+    };
+
+    info!("Response: {:#?}", upstream_response);
+
+    let status = upstream_response.status();
+    let headers = upstream_response.headers().clone();
+
+    let Ok(mut response) = Response::builder()
+        .status(status)
+        .body(Body::from_stream(upstream_response.bytes_stream()))
+    else {
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    };
+
+    *response.headers_mut() = headers;
+
+    response
+}
